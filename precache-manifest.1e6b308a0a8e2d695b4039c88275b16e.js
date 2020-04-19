200d10f337439d537a3158f8eaaf1ffd6b026f7a
Amarjeet Kumar <amarjeet.kumar@BRILLIO.COM>
2020-04-19 13:46:03 +0530

    Updates

---
+++ precache-manifest.1e6b308a0a8e2d695b4039c88275b16e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32c0bca67c2d23e5848a99cb65cd392",
+    "url": "/moke-test/index.html"
+  },
+  {
+    "revision": "544fb6e013e97f188cc4",
+    "url": "/moke-test/static/css/2.1468f438.chunk.css"
+  },
+  {
+    "revision": "83e6d1b7e942fb5c434b",
+    "url": "/moke-test/static/css/main.eba1ede6.chunk.css"
+  },
+  {
+    "revision": "544fb6e013e97f188cc4",
+    "url": "/moke-test/static/js/2.314d9580.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/moke-test/static/js/2.314d9580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e6d1b7e942fb5c434b",
+    "url": "/moke-test/static/js/main.515a90e5.chunk.js"
+  },
+  {
+    "revision": "10b29648a2d734099274",
+    "url": "/moke-test/static/js/runtime-main.e6cc9cb4.js"
+  }
+]);
